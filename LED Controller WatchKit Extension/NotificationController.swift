282c1332a2ddcde51faa39ad4fe7afda09889903
Sriram Alagappan <sriram.v.alagappan@gmail.com>
2020-07-27 21:37:41 -0700

    Initial Commit

---
+++ LED Controller WatchKit Extension/NotificationController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationController.swift
+//  LED Controller WatchKit Extension
+//
+//  Created by Sriram Alagappan on 7/27/20.
+//  Copyright © 2020 Sriram Alagappan. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
