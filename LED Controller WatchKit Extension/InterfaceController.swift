b57ced63a2c89a8111a1e7d7b45a2a0079983b7c
Sriram Alagappan <sriram.v.alagappan@gmail.com>
2020-07-28 20:11:43 -0700

    added buttons + functionality

--- LED Controller WatchKit Extension/InterfaceController.swift
+++ LED Controller WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,57 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet weak var ledLabel: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+    }
+    
+    @IBAction func rainbowButton() {
+        let url     = URL(string: "http://192.168.0.103/rainbow")!
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+        })
+
+        task.resume()
+    }
+    
+    @IBAction func colorButton() {
+        let url     = URL(string: "http://192.168.0.103/blue")!
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+        })
+
+        task.resume()
+    }
+    
+    @IBAction func offButton() {
+        let url     = URL(string: "http://192.168.0.103/off")!
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+        })
+
+        task.resume()
     }
     
     override func willActivate() {
